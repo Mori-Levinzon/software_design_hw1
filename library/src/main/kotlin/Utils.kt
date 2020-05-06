9adaec5c1fb02d97172270a7fd1a1e8a9d5dc2e4
Mori Levinzon <26859371+Mori-Levinzon@users.noreply.github.com>
2020-05-06 16:52:45 +0300

    Utils functions implemented and tested except hexEncode which is from the standard libary

--- library/src/main/kotlin/Utils.kt
+++ library/src/main/kotlin/Utils.kt
@@ -1,10 +1,14 @@
 package il.ac.technion.cs.softwaredesign
 
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 import java.lang.StringBuilder
 import java.security.MessageDigest
 import kotlin.random.Random
+import java.util.*
+import kotlin.Comparator
 
-class Utils {
+class Utils: Comparator<String> {
 
     companion object{
 
@@ -16,7 +20,9 @@ class Utils {
             return bytes.fold("", { str, it -> str + "%02x".format(it) })
         }
 
-        fun hexEncode(str:String):String=TODO("Implement me!")
+        fun hexEncode(str:String):String {
+            return URLEncoder.encode(str, StandardCharsets.UTF_8.toString())
+        }
 
         fun getRandomChars(length: Int):String {
             val allowedChars : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
@@ -25,7 +31,23 @@ class Utils {
                     .map(allowedChars::get).joinToString("")
         }
 
-        fun compareIPs(ip1: String, ip2 : String):String=TODO("Implement me!")
+        fun compareIPs(ip1: String?, ip2: String?):Int {
+            try {
+                if (ip1 == null || ip1.toString().isEmpty()) return -1
+                if (ip2 == null || ip2.toString().isEmpty()) return 1
+                val ba1: List<String> = ip1.split(".")
+                val ba2: List<String> = ip2.split(".")
+                for (i in ba1.indices) {
+                    val b1 = ba1[i].toInt()
+                    val b2 = ba2[i].toInt()
+                    if (b1 == b2) continue
+                    return if (b1 < b2) -1 else 1
+                }
+                return 0
+            } catch (ex: Exception) {
+                return 0
+            }
+        }
 
         /**
          * If the response is compact, return string as-is. Otherwise, turn the non-compact list
@@ -33,5 +55,9 @@ class Utils {
          */
         fun getPeersStringFromResponse(response: Map<String, Any>):String =TODO("Implement me! @Bahjat")
 
+    }
+
+    override fun compare(ip1: String?, ip2: String?): Int {
+        return compareIPs(ip1,ip2)
     }
 }
---
+++ library/src/test/kotlin/UtilsTest.kt
@@ -0,0 +1,50 @@
+
+
+import com.google.common.base.Predicates.equalTo
+import il.ac.technion.cs.softwaredesign.Utils
+import il.ac.technion.cs.softwaredesign.Utils.Companion.getRandomChars
+import il.ac.technion.cs.softwaredesign.Utils.Companion.hexEncode
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
+
+class UtilsTest {
+
+    @Test
+    fun `IP compares correctly`() {
+        val addresses = ArrayList<String>()
+        addresses.add("123.4.245.23")
+        addresses.add("104.244.253.29")
+        addresses.add("1.198.3.93")
+        addresses.add("32.183.93.40")
+        addresses.add("104.30.244.2")
+        addresses.add("104.244.4.1")
+
+        val sortedAddresses = ArrayList<String>()
+        sortedAddresses.add("1.198.3.93")
+        sortedAddresses.add("32.183.93.40")
+        sortedAddresses.add("104.30.244.2")
+        sortedAddresses.add("104.244.4.1")
+        sortedAddresses.add("104.244.253.29")
+        sortedAddresses.add( "123.4.245.23")
+        val utils = Utils()
+        addresses.sortWith(utils)
+        Assertions.assertEquals(addresses, sortedAddresses)
+    }
+
+    @Test
+    fun `Random Char return characters in range and in correct length`() {
+        val randomStr = getRandomChars(6)
+        Assertions.assertEquals(randomStr.length, 6)
+        for (i in randomStr.indices){
+            assert(randomStr[i] in 'a'..'z' || randomStr[i] in 'A'..'Z' || randomStr[i] in '0'..'9')
+        }
+
+        assert( getRandomChars(0) == "")
+
+        assertThrows<IllegalArgumentException> { getRandomChars(-1) }
+
+    }
+
+}
